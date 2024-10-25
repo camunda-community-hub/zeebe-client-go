3258105613d3c59c61c89d91ff8e6ba3608b4c9f
Max Danilov <maxdanilov@users.noreply.github.com>
2024-10-25 21:15:28 +0200

    build: include Darwin ARM64 build

--- cmd/zbctl/build.sh
+++ cmd/zbctl/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xeu
 
-OS=( linux windows darwin )
-BINARY=( zbctl zbctl.exe zbctl.darwin )
+OS=( linux windows darwin darwin )
+ARCH=( amd64 amd64 amd64 arm64 )
+BINARY=( zbctl zbctl.exe zbctl.darwin-amd64 zbctl.darwin-arm64 )
+
 SRC_DIR=$(dirname "${BASH_SOURCE[0]}")
 DIST_DIR="$SRC_DIR/dist"
 
@@ -13,7 +15,7 @@ rm -rf ${DIST_DIR}/*
 
 for i in "${!OS[@]}"; do
 	if [ $# -eq 0 ] || [ ${OS[$i]} = $1 ]; then
-	    CGO_ENABLED=0 GOOS="${OS[$i]}" GOARCH=amd64 go build -a -tags netgo -ldflags "-w -X github.com/camunda-community-hub/zeebe-client-go/v8/cmd/zbctl/internal/commands.Version=${VERSION} -X github.com/camunda-community-hub/zeebe-client-go/v8/cmd/zbctl/internal/commands.Commit=${COMMIT}" -o "${DIST_DIR}/${BINARY[$i]}" "${SRC_DIR}/main.go" &
+	    CGO_ENABLED=0 GOOS="${OS[$i]}" GOARCH="${ARCH[$i]}" go build -a -tags netgo -ldflags "-w -X github.com/camunda-community-hub/zeebe-client-go/v8/cmd/zbctl/internal/commands.Version=${VERSION} -X github.com/camunda-community-hub/zeebe-client-go/v8/cmd/zbctl/internal/commands.Commit=${COMMIT}" -o "${DIST_DIR}/${BINARY[$i]}" "${SRC_DIR}/main.go" &
 	fi
 done
 
